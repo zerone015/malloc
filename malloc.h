8d5b067f3294f9e6be56ac6304f88e1bca69628e
Youngwon Son <yoson@c6r10s7.42seoul.kr>
2023-07-21 15:19:47 +0900

    feat: 헤더 재정의

--- malloc.h
+++ malloc.h
@@ -3,35 +3,36 @@
 
 #include <stddef.h>
 #include <pthread.h>
+#include <errno.h>
 
+#define chunkptr            struct t_chunk*
+#define ALLOC_MAX_SIZE              
 #define SIZE_SZ             (sizeof (size_t))
 #define MALLOC_ALIGNMENT    16
 #define MALLOC_ALIGN_MASK   (MALLOC_ALIGNMENT - 1)
 #define MIN_CHUNK_SIZE      32
 #define MIN_SMALL_SIZE      1024
 #define MIN_LARGE_SIZE      128 * 1024
-#define chunkptr            struct chunk *
 
-struct state
-{
+typedef struct s_state {
     pthread_mutex_t mutex;
 
-    chunkptr top;
+    chunkptr  top;
 
-    chunkptr last_remainder;
+    chunkptr  last_remainder;
     
-    chunkptr bins[254];
-};
+    chunkptr  bins[254];
+} t_state;
 
-struct chunk {
-  size_t      prev_size;  
-  size_t      size;       
+struct t_chunk {
+  size_t  prev_size;  
+  size_t  size;       
 
-  struct chunk* fd;         
-  struct chunk* bk;
+  struct t_chunk  *fd;
+  struct t_chunk  *bk;
 
-  struct chunk* fd_nextsize;
-  struct chunk* bk_nextsize;
+  struct t_chunk  *fd_nextsize;
+  struct t_chunk  *bk_nextsize;
 };
 
 #endif
